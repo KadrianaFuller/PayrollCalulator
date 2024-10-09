203471288a4f3d9bc79c7aa023896b44a12b672e
kadrianafuller <kadrianafuller@icloud.com>
2024-10-09 16:34:02 -0500

    second commit

--- src/main/java/com/Pluralsight/Main.java
+++ src/main/java/com/Pluralsight/Main.java
@@ -3,18 +3,28 @@ package com.Pluralsight;
 
 import com.Pluralsight.Employee;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Please enter the name of the employee file to process: ");
+        String fileName = in.nextLine();
+
+        System.out.print("Please enter the name of the payroll file to create: ");
+        String payrollFile= in.nextLine();
+
         try {
 
-            FileReader fileReader = new FileReader("src/main/resources/employees.csv");
-
+            FileReader fileReader = new FileReader("src/main/resources/" + fileName);
             BufferedReader bufReader = new BufferedReader(fileReader);
+
+            FileWriter fileWriter= new FileWriter("src/main/resources/" + payrollFile);
+            BufferedWriter bufWrite = new BufferedWriter(fileWriter);
+
             String input;
 
             while ((input = bufReader.readLine()) != null) {
@@ -27,15 +37,20 @@ public class Main {
                 Employee employee = new Employee(id,name,hours,rate);
 
 
-                System.out.printf("""
-                Employee Info:
-                Id: %s
-                Name: %s
-                Gross Pay: %.2f
-             """,employee.getEmployeeID(),employee.getName(), employee.getGrossPay());
+//                System.out.printf("""
+//                Employee Info:
+//                Id: %s
+//                Name: %s
+//                Gross Pay: %.2f
+//             """,employee.getEmployeeID(),employee.getName(), employee.getGrossPay());
+//
+             bufWrite.write(String.format("%d |%s |%.2f \n", employee.getEmployeeID(), employee.getName(), employee.getGrossPay()));
+
             }
+            bufWrite.close();
+
             bufReader.close();
-            fileReader.close();
+
 
         } catch (IOException e) {
             // display stack trace if there was an error
